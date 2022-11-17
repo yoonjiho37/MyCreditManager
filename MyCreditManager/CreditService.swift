468eeb280e49028bc7c2cfcb64693e6630f79002
yoonjiho37 <yoon.jiho37@gmail.com>
2022-11-17 16:08:24 +0900

    [Feature] 학생 Remove 기능 추가

--- MyCreditManager/CreditService.swift
+++ MyCreditManager/CreditService.swift
@@ -15,24 +15,38 @@ class CreditService {
         switch selected {
         case "1":
             addStudent()
+        case "2":
+            removeStudent()
         default: break
         }
     }
     
     static private func addStudent() {
         let input = readLine()
-        let studentinput = String(input ?? "")
+        let studentInput = String(input ?? "")
         
 
-        if students.filter({ $0.name == studentinput })[safe: 0] == nil {
-            let student = CreditModel(name: studentinput,credit: nil)
+        if students.filter({ $0.name == studentInput })[safe: 0] == nil {
+            let student = CreditModel(name: studentInput,credit: nil)
             students.append(student)
-            print("\(studentinput) 학생을 추가했습니다.")
+            print("\(studentInput) 학생을 추가했습니다.")
         } else {
-            print("\(studentinput)은 이미 있는 학생입니다. 추가하지 않습니다.")
+            print("\(studentInput)은 이미 있는 학생입니다. 추가하지 않습니다.")
         }
+    }
+
+    
+    static private func removeStudent() {
+        let input = readLine()
+        let studentInput = String(input ?? "")
         
-        
+        if students.filter({ $0.name == studentInput})[safe: 0] != nil {
+            guard let removeIndex = students.firstIndex(where: {$0.name == studentInput}) else { return }
+            students.remove(at: removeIndex)
+            print("\(studentInput) 학생을 삭제하였습니다.")
+        } else {
+            print("\(studentInput) 삭제할 학생의 이름을 찾지 못했습니다.")
+        }
     }
     
     
